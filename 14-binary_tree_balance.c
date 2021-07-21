9b8a2da61e0eb10cfa50db5ed77a25baec6e6539
01Eddie <jesus.11.97@hotmail.com>
2021-07-21 03:35:02 -0500

    [ADD] files advanced and some mandatorys

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree);
 
 /**
  * binary_tree_balance - Entry point
@@ -13,9 +14,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_left = 1 + binary_tree_balance(tree->left);
+		height_left = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		height_right = 1 + binary_tree_balance(tree->right);
+		height_right = 1 + binary_tree_height(tree->right);
 
 	return (height_left - height_right);
+}
+
+/**
+ * binary_tree_height - Entry point
+ * @tree: Constant of binary_tree_t
+ * Return: rightSide or leftSide
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int leftSide = 0, rightSide = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	leftSide = 1 + binary_tree_height(tree->left);
+	rightSide = 1 + binary_tree_height(tree->right);
+
+	if (leftSide > rightSide)
+		return (leftSide);
+	return (rightSide);
 }
